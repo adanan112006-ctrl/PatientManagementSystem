592404dff8ec3f7309083ea1323ecf37aeaaafae
adanan112006@gmail.com <adanan112006@gmail.com>
2026-06-21 23:29:34 +0530

    Update app.py
    Added patient storage and view patients feature

--- app.py
+++ app.py
@@ -1,4 +1,37 @@
-patients =[]
+patients = []
+
 while True:
-    print("\n=== Patient Management Syystem ===")
-    print("")
+    print("\n=== Patient Management System ===")
+    print("1. Add Patient")
+    print("2. View Patients")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("Add Patient")
+
+        name = input("Enter patient name: ")
+        age = int(input("Enter patient age: "))
+
+        patient = {
+            "name": name,
+            "age": age
+        }
+
+        patients.append(patient)
+
+        print("Patient added successfully!")
+
+    elif choice == "2":
+        print("\nPatients:")
+
+        for patient in patients:
+            print(patient["name"], patient["age"],)
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid Choice")
