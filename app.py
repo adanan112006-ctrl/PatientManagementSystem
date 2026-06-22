e3ac58e77554cefdac8fe4da10d2910e926c1d28
adanan112006@gmail.com <adanan112006@gmail.com>
2026-06-22 10:35:33 +0530

    Update app.py
    Added delete patient feature

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ while True:
     print("\n=== Patient Management System ===")
     print("1. Add Patient")
     print("2. View Patients")
-    print("3. Exit")
+    print("3. Delete Patient")
+    print("4. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -27,9 +28,20 @@ while True:
         print("\nPatients:")
 
         for patient in patients:
-            print(patient["name"], patient["age"],)
+            print(patient["name"], patient["age"])
 
     elif choice == "3":
+        print("\npatients")
+        
+        for i in range(len(patients)):
+            print(i + 1, patients[i]["name"] )
+
+        delete_number = int(input("Enter Patient number to delete: "))
+        del patients[delete_number -1]
+
+        print("Patient deleted successfully!")
+
+    elif choice == "4":
         print("Goodbye!")
         break
 
